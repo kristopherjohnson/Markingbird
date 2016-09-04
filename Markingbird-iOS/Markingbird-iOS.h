5f9455e5814ef6992f8282aad7d657d9c9902555
Romain Pouclet <romain.pouclet@gmail.com>
2016-09-03 21:30:48 -0400

    Add iOS scheme

---
+++ Markingbird-iOS/Markingbird-iOS.h
@@ -0,0 +1,19 @@
+//
+//  Markingbird-iOS.h
+//  Markingbird-iOS
+//
+//  Created by Romain Pouclet on 2016-09-03.
+//  Copyright © 2016 Kristopher Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Markingbird-iOS.
+FOUNDATION_EXPORT double Markingbird_iOSVersionNumber;
+
+//! Project version string for Markingbird-iOS.
+FOUNDATION_EXPORT const unsigned char Markingbird_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Markingbird_iOS/PublicHeader.h>
+
+
